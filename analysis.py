05fc34945709bb9da7e98047934cbc66cac18d44
Omer Palevitch <Omer.Pa@Solaredge.com>
2023-10-06 22:01:46 +0300

    finish analysis.py and symnmf.py

--- analysis.py
+++ analysis.py
@@ -1,113 +1,58 @@
 import math
 import sys
 import numpy as np
-import mysymnmf as sm
-import pandas as pd
+#import mysymnmf as sm
+from sklearn.metrics import silhouette_score
 
 def main():
     np.random.seed(0)
     N = len(sys.argv)
     if (N == 3):
-        k = sys.argv[1]
+        k = float(sys.argv[1])
+        if (k.is_integer()):
+            k = int(k)
         path = sys.argv[2]
     else:
         print("An Error Has Occurred")
         return
-    clusters_kmean = Hw1(path)
-    file_mat = np.loadtxt(path)
-    rows = file_mat.shape[0]
-    col = file_mat.shape[0][0]
-    W = sm.norm(file_mat, rows, col)
-    W_array = np.array((W))
+    points_nmf = read_file_nmf(path)
+    centroids_kmean = Hw1(k,points_nmf)
+    clusters_kmean_label = find_clusters(points_nmf,k,centroids_kmean)
+    rows = len(points_nmf[0])
+    col = len(points_nmf)
+    #W = sm.norm(file_mat, rows, col)
+    path_norm = "C:/Users/omer.pa/Desktop/school/third_year/second_semester/Software_project/symnmf/norm.txt"
+    W=read_file_nmf(path_norm)
+    W_array = np.array(W)
     n = W_array.shape[0]
     m = np.mean(W_array)
     r = math.sqrt(m / k)
     H = np.random.uniform(low=0, high=r, size=(n, k))
-    H_new = sm.symnmf(H, W, n, k)
-    clusters_symnmf = [n][n]
-    for i in range(n):
-        max_in_row = H_new[i][0]
-        max_index = 0
-        for j in range(1,n):
-            if(H_new[i][j]>max_in_row):
-                max_in_row = H_new[i][j]
-                max_index = j
-        clusters_symnmf[max_index] = clusters_symnmf[max_index] + H_new[i]
-    a_kmeans = find_a(clusters_kmean)
-    b_kmeans = find_b(clusters_kmean)
-    Sil_kmeans = find_sil(a_kmeans,b_kmeans)
-    a_symnmf = find_a(clusters_symnmf)
-    b_symnmf = find_b(clusters_symnmf)
-    Sil_symnmf = find_sil(a_symnmf, b_symnmf)
-    print("nmf: " + Sil_symnmf)
-    print("kmeans: " + Sil_kmeans)
+    #H_new = sm.symnmf(H, W, n, k)
+    H_path = "C:/Users/omer.pa/Desktop/school/third_year/second_semester/Software_project/symnmf/H_path.txt"
+    H_new = read_file_nmf(H_path)
+    H_new_pd = np.array(H_new)
+    labels_nmf = np.argmax(H_new_pd, axis=1)
+    Sil_symnmf = silhouette_score(points_nmf,labels_nmf)
+    Sil_kmeans = silhouette_score(points_nmf,clusters_kmean_label)
+    print("nmf: " + str(round(Sil_symnmf,4)))
+    print("kmeans: " + str(round(Sil_kmeans,4)))
 
 
+def add_to_clusters(cluster_num,row,clusters):
+    for i in range(len(row)):
+        clusters[cluster_num].append(row[i])
 
-
-
-if __name__ == "__main__":
-    main()
-def find_sil(a,b):
-    return ((b-a)/max(a,b))
-def find_a(clusters):
-    sum = 0
-    iter = 0
-    for i in range(len(clusters)):
-        for j in range(len(clusters[i])):
-            for k in range(len(clusters[i])):
-                if(k!=j):
-                    iter +=1
-                    sum+=(clusters[i][j]-clusters[i][k])
-    return sum/iter
-
-def find_b(clusters):
-    sum = 0
-    iter = 0
-    for i in range(len(clusters)):
-        for j in range(len(clusters[i])):
-            for k in range(clusters[i]):
-                for m in range(len(clusters[i])):
-                    if (k != i):
-                        iter += 1
-                        sum += (clusters[i][j] - clusters[k][m])
-    return sum / iter
-
-def Hw1(path):
-    arr = read_file(path)
-    N = len(arr)
-    try:
-        K = validateK(N)
-    except(AssertionError):
-        print("Invalid number of clusters!")
-        return
+def Hw1(k,points):
     iteration = 300
     try:
-        clusters = k_means(K,iteration,arr)
+        centroids = k_means(k,iteration,points)
     except(AssertionError):
         print("An Error Has Occurred")
         return
-    return clusters
+    return centroids
 
-
-
-    #make sure k is valid
-def validateK(N):
-    try:
-        k = float(sys.argv[0])
-        if(k.is_integer()):
-            k = int(k)
-        else:
-            assert 1 ==0
-        if (k<1 or k>N):
-            assert 1 ==0
-    except:
-        assert 1 == 0
-    else:
-        return k
-
-
-def read_file(path):
+def read_file_nmf(path):
     file = open(path,"r")
     line = file.readline()
     arr=[]
@@ -115,11 +60,10 @@ def read_file(path):
         temp = []
         for word in line.split(","):
             temp.append(float(word))
-        arr.append([tuple(temp)])
+        arr.append(temp)
         line = file.readline()
     file.close()
     return arr
-
 
 #calculate euclidean_distance
 def euclidean_distance(p, q):
@@ -139,42 +83,44 @@ def find_min_cluster(p,clusters):
             min_index = i
     return min_index
 
-def k_means(k,iter_max,arr):
-    clusters = [0] * k
-    #initialize clusters
-    for i in range(k):
-        clusters[i] = [arr[i][0],[]]
-    smaller_then_eps = False
+def k_means(k,iter_max,points):
+    prev_centroid = points[:k]
+    update_dist = euclidean_distance(prev_centroid[0],prev_centroid[1])
+    centroids = []
     iter_count = 0
-    while(iter_count<iter_max and smaller_then_eps==False):
-        for p in arr:
-            index = find_min_cluster(p,clusters)
-            if(len(p) !=1):
-                clusters[p[1]][1].remove(p[0])
-                p[1] = index
-            else:
-                p.append(index)
-            clusters[index][1].append(p[0])
-        smaller_then_eps = update_centroids(k,smaller_then_eps,clusters)
-        iter_count+=1
-    return clusters
+    while(iter_count<iter_max and update_dist>0.0001):
+        iter_count +=1
+        clusters = find_clusters(points,k,prev_centroid)
+        centroids = update_centroids(points,k,clusters)
+        update_dist = dist_cent(prev_centroid,centroids)
+        prev_centroid = centroids
+    return centroids
 
-def update_centroids(k,smaller_then_eps,clusters):
-    eps = math.exp(-4)
-    max_dist = 0
+def dist_cent(prev_centroids, new_centroids):
+    sum = 0
+    for prev, new in zip(prev_centroids, new_centroids):
+        sum += euclidean_distance(prev, new)
+    return sum
+def find_clusters(points,k,prev_centroid):
+    clusters_points = [0] * len(points)
+    for i in range(len(points)):
+        min_index = 0
+        min_dist = euclidean_distance(points[i],prev_centroid[0])
+        for j in range(k):
+            curr_dist = euclidean_distance(points[i],prev_centroid[j])
+            if(curr_dist<min_dist):
+                min_dist = curr_dist
+                min_index = j
+        clusters_points[i] = min_index
+    return clusters_points
+def update_centroids(points,k,clusters):
+    new_centroid = [0] * k
     for i in range(k):
-        old_means= clusters[i][0]
-        new_means = []
-        for j in range(len(old_means)):
-            cluster_size = len(clusters[i][1])
-            sum1 = 0
-            for k in clusters[i][1]:
-                sum1+=k[j]
-            new_means.append(sum1/cluster_size)
-        clusters[i][0] = new_means
-        dist = euclidean_distance(old_means,new_means)
-        if dist>max_dist:
-            max_dist = dist
-    if(max_dist<eps):
-        smaller_then_eps = True
-    return smaller_then_eps
+        coor_data_points = [points[j] for j in range(len(points)) if clusters[j] == i]
+        points_amount = len(coor_data_points)
+        new_centroid[i] = list(sum(coord) / points_amount for coord in zip(*coor_data_points))
+    return new_centroid
+
+
+if __name__ == "__main__":
+    main()
--- symnmf.py
+++ symnmf.py
@@ -2,6 +2,7 @@ import math
 import sys
 import mysymnmf as sm
 import numpy as np
+import pandas as pd
 
 def main():
     num_arg = len(sys.argv)
@@ -12,28 +13,34 @@ def main():
         k = float(sys.argv[1])
         goal = sys.argv[2]
         text_file = sys.argv[3]
-    file_mat = np.loadtxt(text_file)
-    rows = file_mat.shape[0]
-    col = file_mat.shape[0][0]
+    file_mat = pd.read_csv(text_file,header=None).values.tolist()
+    rows = len(file_mat[0]) if (file_mat is not None) else 0
+    col = len(file_mat) if (file_mat is not None) else 0
     if(goal == "symnmf"):
         np.random.seed(0)
         W = sm.norm(file_mat,rows,col)
-        W_array = np.array((W))
-        n = W_array.shape[0]
-        m = np.mean(W_array)
+        m = np.mean(W)
         r = math.sqrt(m / k)
-        H = np.random.uniform(low=0, high=r, size=(n, k))
-        sm.symnmf(H,W,n,k)
+        H = np.random.uniform(low=0, high=r, size=(col, k))
+        H_new = sm.symnmf(H,W,n,k)
+        print_matrix(H_new)
         return
     elif(goal == "sym"):
-        sm.sym(file_mat,rows,col)
+        A = sm.sym(file_mat,rows,col)
+        print_matrix(A)
     elif(goal == "dgd"):
-        sm.dgd(file_mat,rows,col)
+        D = sm.dgd(file_mat,rows,col)
+        print_matrix(D)
     elif(goal == "norm"):
-        sm.norm(file_mat,rows,col)
+        W = sm.norm(file_mat,rows,col)
+        print_matrix(W)
     else:
         print("An Error Has Occurred")
         return
+def print_matrix(matrix):
+    for row in matrix:
+        row_round= [f'{item:.4f}' for item in row]
+        print(','.join(row_round))
 
 if __name__ == "__main__":
     main()
